64d1317c845a41be8b080be23c341c6a35988e1c
JuanPerezAnzaldo <juan.perez.anzaldo@uabc.edu.mx>
2026-09-05 23:56:14 +0000

    s

--- chambeki/user/includes/header.php
+++ chambeki/user/includes/header.php
@@ -8,9 +8,9 @@
                 <link rel="manifest" href="/manifest.json">
                 <meta name="theme-color" content="#ff682e">
 
-                <link rel="stylesheet" href="<?php echo CSS_RUTA; ?>global.css">
+                <link rel="stylesheet" href="<?php echo CSS_RUTA; ?>global.css?=v2">
                 <?php if (!isset($accion) || $accion === 'inicio'): ?>
-                    <link rel="stylesheet" href="<?php echo CSS_RUTA; ?>home.css">
+                    <link rel="stylesheet" href="<?php echo CSS_RUTA; ?>home.css?=v2">
                 <?php endif; ?>
             </head>
             <body>
