206fd08c7bf5a050ad5a89df8661df2c354fa37e
Santiago Almanza <65076527-shantiac27@users.noreply.replit.com>
2026-09-11 02:46:19 +0000

    prueba

--- index.php
+++ index.php
@@ -28,4 +28,34 @@
     }
     
     include(FOOTER);
+?><?php
+    //ingnoren esto, es para borrar los comentarios al inspeccionar la pagina
+    ob_start(function($buffer)
+    {
+        return preg_replace('/<!--(.|\s)*?-->/', '', $buffer);
+    });
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/chambeki/shared/includes/configuration.php';
+    
+    $accion = isset($_GET['accion']) ? trim($_GET['accion']) : 'inicio';
+    
+    include(HEADER);
+    
+    switch ($accion)
+    {
+        case 'servicios':
+        {
+            include(PAGINAS . 'servicios.php');
+            break;
+        }
+    
+        case 'inicio':
+        default:
+        {
+            include(PAGINAS . 'home.php');
+            break;
+        }
+    }
+    
+    include(FOOTER);
 ?>
